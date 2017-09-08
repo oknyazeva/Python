2a8ea7b78bd0400b6bde4d2f62576d8bd9939066
o_knyazeva <kniazeva.volha@gmail.com>
2017-09-08 11:44:38 +0300

    [add_Tasks_List1,2,3]

---
+++ Tasks/Chapter2/Lists/List1.py
@@ -0,0 +1,7 @@
+list = [2, 1, 3, 4, 5]
+t = tuple(list)
+print ("Max =", max(t))
+print ("Min =", min(t))
+print("Sum =", sum(t))
+rt = sorted(t, reverse=True)
+print("Sort =", rt)
---
+++ Tasks/Chapter2/Lists/List2.py
@@ -0,0 +1,9 @@
+t = tuple("Hello World!")
+print ("string =",''.join(t))
+print t.count(t[0])
+l = list(t)
+print ("List =", l)
+
+for x in range(1,len(l), 2):
+    l[x] = -1
+print l
